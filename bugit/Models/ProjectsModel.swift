20ba0c6d987c090cdff7b99f144088fba40517b9
Bipin Pattan <bpattan@couponsinc.com>
2016-11-30 00:09:23 -0800

    Add a model for Jira Projects.

---
+++ bugit/Models/ProjectsModel.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectsModel.swift
+//  bugit
+//
+//  Created by Bipin Pattan on 11/30/16.
+//  Copyright © 2016 BugIt App. All rights reserved.
+//
+
+import UIKit
+
+class ProjectsModel: NSObject {
+    var avatarImageLarge: URL?
+    var avatarImageSmall: URL?
+    var id: Int?
+    var key: String?
+    var name: String?
+}
