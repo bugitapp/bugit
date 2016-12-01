fe5b80b23d6b6bb3c3e8de30a852e02552b438dc
Ernest Semerda <ernest@semerda.com>
2016-11-30 21:21:47 -0800

    Intro Guide.

---
+++ bugit/IntroGuide/Step3ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  Step3ViewController.swift
+//  bugit
+//
+//  Created by Ernest on 11/30/16.
+//  Copyright © 2016 BugIt App. All rights reserved.
+//
+
+import UIKit
+
+class Step3ViewController: ViewController {
+
+    @IBOutlet weak var closeIntroButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    // MARK: - Actions
+    
+    @IBAction func closeIntro(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
