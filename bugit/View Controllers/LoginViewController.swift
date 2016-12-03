9155962a71788b14081477a4706294c9a3abc0b5
Bipin Pattan <bpattan@couponsinc.com>
2016-12-03 13:41:36 -0800

    Add more updated pens so they show better when background matches pen color.

--- bugit/View Controllers/LoginViewController.swift
+++ bugit/View Controllers/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MBProgressHUD
 
 class LoginViewController: UIViewController {
 
@@ -33,20 +34,13 @@ class LoginViewController: UIViewController {
         jiraMgr?.projects(
             success: { (projects: [ProjectsModel]) in
                 print("Projects: \(projects)")
+                MBProgressHUD.hide(for: self.view, animated: true)
+                _ = self.navigationController?.popViewController(animated: true)
             },
             failure: { (error: NSError) in
                 print("Error : \(error)")
+                MBProgressHUD.hide(for: self.view, animated: true)
         })
-    }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+        MBProgressHUD.showAdded(to: self.view, animated: true)
+    }    
 }
