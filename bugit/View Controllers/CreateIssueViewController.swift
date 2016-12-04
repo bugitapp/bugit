40d0f88c85227c61f5139626b217c2b6af3196d2
Bipin Pattan <bpattan@couponsinc.com>
2016-12-04 00:33:55 -0800

    Add  cells to the create issue VC to add text.

--- bugit/View Controllers/CreateIssueViewController.swift
+++ bugit/View Controllers/CreateIssueViewController.swift
@@ -13,10 +13,11 @@ class CreateIssueViewController: UITableViewController {
     var project: ProjectsModel?
     var issueType: IssueTypeModel?
     var issuePriority: PriorityTypeModel?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupUI()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -24,6 +25,11 @@ class CreateIssueViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
+    func setupUI() {
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 100
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -33,31 +39,53 @@ class CreateIssueViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        return 4
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 3
+        if section == 0 {
+            return 3
+        } else if section == 1 || section == 2 || section == 3  {
+            return 1
+        }
+        return 0
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if (indexPath.row == 0) {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "SelectionCell", for: indexPath) as! SelectionCell
-            cell.config(key: "Project", value: "-")
-            return cell
-        } else if (indexPath.row == 1) {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "SelectionCell", for: indexPath) as! SelectionCell
-            cell.config(key: "Type", value: "-")
-            return cell
-        } else if (indexPath.row == 2) {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "SelectionCell", for: indexPath) as! SelectionCell
-            cell.config(key: "Priority", value: "-")
+        if indexPath.section == 0 {
+            if (indexPath.row == 0) {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "SelectionCell", for: indexPath) as! SelectionCell
+                let projValue = project?.key == nil ? "-" : project?.key
+                cell.config(key: "Project", value: projValue)
+                return cell
+            } else if (indexPath.row == 1) {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "SelectionCell", for: indexPath) as! SelectionCell
+                let typeValue = issueType?.name == nil ? "-" : issueType?.name
+                cell.config(key: "Type", value: typeValue)
+                return cell
+            } else if (indexPath.row == 2) {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "SelectionCell", for: indexPath) as! SelectionCell
+                let priorityValue = issuePriority?.name == nil ? "-" : issuePriority?.name
+                cell.config(key: "Priority", value: priorityValue)
+                return cell
+            }
+        } else if indexPath.section == 1 || indexPath.section == 2 || indexPath.section == 3  {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "InfoCell", for: indexPath) as! TextViewCell
             return cell
         }
         return UITableViewCell()
     }
 
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 1 {
+            return "Summary"
+        } else if section == 2 {
+            return "Description"
+        } else if section == 3 {
+            return "Environment"
+        }
+        return ""
+    }
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
